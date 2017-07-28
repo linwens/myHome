9d7a36e073e788623950e14f3b262973e3b6c417
linwens <lws200841001@gmail.com>
2017-07-28 20:48:27 +0800

    增加后台管理路由

--- routes/index.js
+++ routes/index.js
@@ -23,6 +23,10 @@ router.get('/blog', function(req, res, next) {
 router.get('/blog/detail', function(req, res, next) {
   res.render('blog/detail', { title: 'Blog详情页' });
 });
+//后台页面
+router.get('/admin', function(req, res, next) {
+  res.render('vue', { title: '后台系统' });
+});
 //测试页面
 router.get('/test', function(req, res, next) {
   res.render('test', { title: '测试' });
