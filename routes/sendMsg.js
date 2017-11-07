dc826071485bed2fe645e0b7030391a196ebb028
linwens <lws200841001@gmail.com>
2017-11-07 16:47:01 +0800

    增加公众号模板消息推送（限测试环境）

--- routes/sendMsg.js
+++ routes/sendMsg.js
@@ -1,48 +1,46 @@
+/*
+ *这个模块用于给用户发送模板消息，个人订阅号无此权限
+**/
 var WechatAPI = require('wechat-api');//WeChatapi封装模块
 var config = require('./config');//配置参数
+var api = new WechatAPI(config.appid, config.appsecret);
 
-//设置所属行业
-var industryIds = {
-	"industry_id1":'39'
-}
-api.setIndustry(industryIds,function(err, rslt, next){
-	console.log('设置所属行业回调');
-})
-//获取模板id
-var templateIdShort = 'TM00015';
-api.addTemplate(templateIdShort, function(err, rslt, next){
-	console.log('获取模板id');
-});
-//获取关注者列表
-api.getFollowers(function(err, rslt, next){
-	console.log('获取关注者列表');
-});
-//发送模板消息
-var templateId: '模板id';
-// URL置空，则在发送后,点击模板消息会进入一个空白页面（ios）, 或无法点击（android）
-var url: '';
-var data = {
-   "first": {
-     "value":"恭喜你购买成功！",
-     "color":"#173177"
-   },
-   "keyword1":{
-     "value":"巧克力",
-     "color":"#173177"
-   },
-   "keyword2": {
-     "value":"39.8元",
-     "color":"#173177"
-   },
-   "keyword3": {
-     "value":"2014年9月22日",
-     "color":"#173177"
-   },
-   "remark":{
-     "value":"欢迎再次购买！",
-     "color":"#173177"
-   }
+exports.wcNotice = function(opt, cb){
+    //发送模板消息
+    var templateId = opt.templateId;
+    var openID = opt.openid;//用户唯一openid
+    // URL置空，则在发送后,点击模板消息会进入一个空白页面（ios）, 或无法点击（android）
+    var url = opt.redUrl;
+    var data = {
+        "first": {
+            "value": opt.data.first,
+            "color":"#173177"
+        },
+        "keyword1":{
+            "value": opt.data.keyword1,
+            "color":"#173177"
+        },
+        "keyword2": {
+            "value": opt.data.keyword2,
+            "color":"#173177"
+        },
+        "keyword3": {
+            "value": opt.data.keyword3,
+            "color":"#173177"
+        },
+        "keyword4": {
+            "value": opt.data.keyword4,
+            "color":"#173177"
+        },
+        "remark":{
+            "value": opt.data.remark,
+            "color":"#173177"
+        }
+    };
+        
+    //发送消息
+    api.sendTemplate(openID, templateId, url, data, function(err, rslt, next){
+        console.log('消息已发出');
+        cb(err, rslt);
+    });
 };
-api.sendTemplate('openid', templateId, url, data, function(err, rslt, next){
-	console.log('消息已发出');
-});
--- routes/wechat.js
+++ routes/wechat.js
@@ -1,30 +1,52 @@
 var express = require('express'); 
 var router = express.Router(); 
-var wechat = require('wechat');//引入微信模块 
-var WechatAPI = require('wechat-api');//WeChatapi封装模块
+var wechat = require('wechat');//引入微信模块
 var config = require('./config');//配置参数
-var menus = require('./menus');//菜单json
+var wcNotice = require('./sendMsg').wcNotice;//发送模板通知模块
+var wcSet = require('./set');//公众号基本配置
 
-var api = new WechatAPI(config.appid, config.appsecret);
-//生成自定义菜单
-api.createMenu(menus,function(err, rslt, next){
-	if(err){
-		console.log(err);
-	}else{
-		console.log(rslt);
-	}
+router.use(express.query());
+//发送模板通知接口
+router.post('/notice',function(req, res, next){
+	var type = req.body.type?String(req.body.type):1;
+	wcSet(type,function(wc_info){
+		var opt = {
+			data:{}
+		};
+		for(var key in wc_info){
+			opt[key] = wc_info[key];
+		}
+		opt.openid = req.body.openid?req.body.openid:'omclkxIWNZj5KiIkj3jorwTMT5sE';
+		opt.redUrl = 'https://www.shy-u.xyz';//通知信息点击重定向地址
+		opt.type = type;
+		opt.data.first = req.body.first?req.body.first:'first';
+		opt.data.keyword1 = req.body.keyword1?req.body.keyword1:'keyword1';
+		opt.data.keyword2 = req.body.keyword2?req.body.keyword2:'keyword2';
+		opt.data.keyword3 = req.body.keyword3?req.body.keyword3:'keyword3';
+		opt.data.keyword4 = req.body.keyword4?req.body.keyword4:'keyword4';
+		opt.data.remark = req.body.remark?req.body.remark:'remark';
+		console.log(opt);
+		wcNotice(opt, function(err, rslt){
+			if(err){
+			    res.json({
+			        code:1,
+			        msg:'消息发送失败'
+			    })
+			}else{
+			    res.json({
+			        code:0,
+			        msg:'消息已发出'
+			    })
+			}
+		});
+	});
 });
-router.use(express.query());//
 router.use('/',wechat(config, function(req, res, next){
 	// 微信输入信息都在req.weixin上 
 	var wcMsg = req.weixin;
-	console.log(wcMsg);
-	console.log(wcMsg.MsgType);
-	console.log(wcMsg.MsgType==='text');
-	//res.reply('Hello world!');
 	//关注公众号时回复
 	if(wcMsg.Event ==='subscribe'){
-		res.reply('感谢亲爱的关注~~');
+		res.reply('感谢你的关注~~');
 	};
 	//自动回复文字消息模板
 	if(wcMsg.MsgType==='text'){
@@ -33,11 +55,18 @@ router.use('/',wechat(config, function(req, res, next){
 				break;
 			case '2': res.reply('看文章');
 				break;
-			case '3': res.reply('玩游戏');
-				break;
+			case '3': res.reply([
+  						{
+						    title: '这是一个小游戏',
+						    description: '这个游戏是用js写的',
+						    picurl: 'https://www.shy-u.xyz/h5static/third/img/data/intro.jpg',
+						    url: 'https://www.shy-u.xyz/h5Demo/third'
+  						}
+					]);
 				break;
 			default: res.reply('亲爱的，发送1 可以查看照片;</br>发送2 可以查看文章;</br>发送3 可以玩游戏;');
 		}
 	}
-}))
+}));
+
 module.exports = router;
